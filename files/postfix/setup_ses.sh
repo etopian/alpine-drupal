b772fe170a0562f8419a9b2c5624b89ce1a55f7b
Sami <etopian@users.noreply.github.com>
2015-09-03 08:28:22 -0600

    Update setup_ses.sh

--- files/postfix/setup_ses.sh
+++ files/postfix/setup_ses.sh
@@ -4,4 +4,4 @@ echo "[$SES_HOST]:$SES_PORT $SES_USER:$SES_SECRET" >> /etc/postfix/sasl_passwd
 postmap hash:/etc/postfix/sasl_passwd
 chown root:root /etc/postfix/sasl_passwd /etc/postfix/sasl_passwd.db
 chmod 0600 /etc/postfix/sasl_passwd /etc/postfix/sasl_passwd.db
-rm /etc/postfix/sasl_passwd
+#rm /etc/postfix/sasl_passwd
